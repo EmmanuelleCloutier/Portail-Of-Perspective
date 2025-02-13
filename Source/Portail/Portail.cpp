c9e1469c0447ab7bdd6853915c2e84c4a52b6561
EmmanuelleCloutier <darkforest1045@gmail.com>
2025-02-13 09:32:44 -0500

    push project into git

---
+++ Source/Portail/Portail.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Portail.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Portail, "Portail" );
+ 
---
+++ Source/Portail/PortailGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PortailGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APortailGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APortailGameMode();
+};
+
+
+
